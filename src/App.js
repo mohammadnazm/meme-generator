7ec1f16c0dfc2942774dd8ad8c8c3c7a8be4588d
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-10 19:43:50 +0300

    fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { useEffect, useState } from "react"
 import "./App.css"
 
 function App() {
+  const [templates, setTemplates] = useState([])
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes").then(x =>
+      x.json().then(response => setTemplates(response.data.memes))
+    )
+  }, [])
+
   return (
     <div className="App">
       <h1>Hi</h1>
