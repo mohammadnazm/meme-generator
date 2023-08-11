d456c74d6d8f53f2c5ad03a2edde5220717e5ac9
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-11 08:25:36 +0300

    upgrade

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import { useEffect, useState } from "react"
 import "./App.css"
 import Meme from "./components/Meme"
 
+const objectToQueryParam = obj => {
+  const params = Object.entries(obj).map(([key, value]) => `${key}=${value}`)
+  return "?" + params.join("&")
+}
+
 function App() {
   const [templates, setTemplates] = useState([])
   const [template, setTemplate] = useState()
+  const [topText, setTopText] = useState()
+  const [bottomText, setBottomText] = useState()
 
   useEffect(() => {
     fetch("https://api.imgflip.com/get_memes").then(x =>
@@ -15,11 +22,35 @@ function App() {
   return (
     <div style={{ textAlign: "center" }}>
       {template && (
-        <form>
+        <form
+          onSubmit={async e => {
+            e.preventDefault()
+            // add logic to create meme from api
+            const params = {
+              template_id: "",
+              text0: topText,
+              text1: bottomText,
+              username: "mhamadnazm",
+              password: "mhamadnazm3@gmail.com",
+            }
+            const response = await fetch(
+              "https://api.imgflip.com/caption_image",
+              { body: JSON.stringify(params) }
+            )
+          }}
+        >
           <Meme template={template} />
-          <input placeholder="top text" />
-          <input placeholder="bottom text" />
-          <button type="button" />
+          <input
+            placeholder="top text"
+            value={topText}
+            onChange={e => setTopText(e)}
+          />
+          <input
+            placeholder="bottom text"
+            value={bottomText}
+            onChange={e => setBottomText(e)}
+          />
+          <button type="button">create meme</button>
         </form>
       )}
       {!template && (
